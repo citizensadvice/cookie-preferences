506d0e00bea8472d99140e480a4958df0168b651
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2025-06-25 12:05:52 +0100

    feat: overriding homepage_url coming from the main app
    we might need to adjust this in the main app - without it, we were getting /assets/?action=index&controller=citizens_advice_cookie_preferences%2Fcookie_preferences_page

--- engine/app/controllers/citizens_advice_cookie_preferences/cookie_preferences_page_controller.rb
+++ engine/app/controllers/citizens_advice_cookie_preferences/cookie_preferences_page_controller.rb
@@ -1,9 +1,22 @@
 module CitizensAdviceCookiePreferences
-  class CookiePreferencesPageController < ::ApplicationController
+  class CookiePreferencesPageController < ::ApplicationController # Inherits from the main application
     # TODO: make it configurable per app
     # public-website application
     layout "application"
 
+    helper_method :homepage_url
+
+    # Fixing issues with main app routes going to the engine
+    include Rails.application.routes.url_helpers
+
     def index; end
+
+    def homepage_url
+      if request.params[:country].present?
+        main_app.extent_root_path(country: request.params[:country])
+      else
+        main_app.root_path
+      end
+    end
   end
 end
