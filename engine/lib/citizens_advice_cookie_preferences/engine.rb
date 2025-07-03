7b3339004e0ae35ffd2e86d2f0f053417b6ac655
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2025-07-03 17:48:19 +0100

    WIP: try pre-compiling assets to make JS files available to host applications

--- engine/lib/citizens_advice_cookie_preferences/engine.rb
+++ engine/lib/citizens_advice_cookie_preferences/engine.rb
@@ -1,5 +1,11 @@
 module CitizensAdviceCookiePreferences
   class Engine < ::Rails::Engine
     isolate_namespace CitizensAdviceCookiePreferences
+
+    # tried precompiling assets to see if that made the JS available for public-website - from https://guides.rubyonrails.org/engines.html#separate-assets-and-precompiling
+    initializer 'citizens_advice_cookie_preferences.assets.precompile' do |app|
+      app.config.assets.precompile += %w( cookie-banner.js application.scss )
+    end
+
   end
 end
