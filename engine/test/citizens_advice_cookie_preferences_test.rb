76081f9cd6aa9782e7c20faab070ec0edcba6b89
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2025-07-16 15:37:54 +0100

    chore: installing and configuring rspec
    Deleted minitest /test folder and repaced it with /spec
    Kept the dummy app

--- engine/test/citizens_advice_cookie_preferences_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CitizensAdviceCookiePreferencesTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert CitizensAdviceCookiePreferences::VERSION
-  end
-end
--- engine/test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
-
-require_relative "../test/dummy/config/ent"
-require "rails/test_help"
-
