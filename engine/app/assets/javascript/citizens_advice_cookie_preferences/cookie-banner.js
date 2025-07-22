f352841a872f07796fabb735a68d0724cd54d43e
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2025-07-22 14:57:20 +0100

    feat: configure cookie preferences gem within demo app Co-authored-by: marianayovcheva <mariana.yovcheva@citizensadvice.org.uk> Co-authored-by: Kath <kath.pobee-norris@citizensadvice.org.uk>

--- engine/app/assets/javascript/citizens_advice_cookie_preferences/cookie-banner.js
+++ engine/app/assets/javascript/citizens_advice_cookie_preferences/cookie-banner.js
@@ -7,86 +7,79 @@ const confirmationMessageAccept = confirmationMessageContainer.querySelector(".j
 const confirmationMessageReject = confirmationMessageContainer.querySelector(".js-cookie-banner__confirmation-reject");
 const hideBannerBtn = confirmationMessageContainer.querySelector("#js-cookie-banner__button-hide");
 
-// current layout of code doesn't update cookies. Will need to change back on Monday. Leaving like this now to aid discussions.
-function initCookieBanner() {
-  function setCookie(cname, cvalue, exdays) {
-    // set expiry date in milliseconds
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-
-  function getCookie(name) {
-    const nameEQ = `${name}=`
-    const cookies = document.cookie.split(';')
-    for (let i = 0, len = cookies.length; i < len; i++) {
-      let cookie = cookies[i]
-      while (cookie.charAt(0) === ' ') {
-        cookie = cookie.substring(1, cookie.length)
-      }
-      if (cookie.indexOf(nameEQ) === 0) {
-        return decodeURIComponent(cookie.substring(nameEQ.length))
-      }
-    }
-    return null
-  }
-
-  const acceptCookies = () => {
-    setCookie('cookie_preference', encodeURIComponent(JSON.stringify({
-      essential: true,
-      additional: true
-    })), 365);
-    setCookie('cookie_preference_set', true, 365);
-    showConfirmationMessage();
-    confirmationMessageAccept.hidden = false;
-  }
-
-  const rejectCookies = () => {
-    setCookie('cookie_preference', encodeURIComponent(JSON.stringify(DEFAULT_COOKIE_CONSENT)), 365);
-    setCookie('cookie_preference_set', true, 365);
-    showConfirmationMessage();
-    confirmationMessageReject.hidden = false;
-  }
-
-  const DEFAULT_COOKIE_CONSENT = {
-    essential: true,
-    additional: false
-  }
-
-  function hideCookieBanner() {
-    cookieBanner.hidden = true;
-  }
-
-  function showConfirmationMessage() {
-    cookieSelectionContainer.hidden = true;
-    confirmationMessageContainer.hidden = false;
-    confirmationMessageContainer.focus();
-  }
-
-  if(getCookie('cookie_preference_set')){
-    hideCookieBanner();
-  }else{
-    setCookie('cookie_preference', encodeURIComponent(JSON.stringify(DEFAULT_COOKIE_CONSENT)), 365);
-  }
-
-  acceptBtn.addEventListener("click", () => {
-    acceptCookies();
-  });
-
-  rejectBtn.addEventListener("click", () => {
-    rejectCookies();
-  });
-
-  hideBannerBtn.addEventListener("click", () => {
-    hideCookieBanner();
-  });
-  console.log("inside the init method");
+function setCookie(cname, cvalue, exdays) {
+  // set expiry date in milliseconds
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-initCookieBanner();
+function getCookie(name) {
+  const nameEQ = `${name}=`
+  const cookies = document.cookie.split(';')
+  for (let i = 0, len = cookies.length; i < len; i++) {
+    let cookie = cookies[i]
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1, cookie.length)
+    }
+    if (cookie.indexOf(nameEQ) === 0) {
+      return decodeURIComponent(cookie.substring(nameEQ.length))
+    }
+  }
+  return null
+}
 
-export default function initCookieBanner();
+const acceptCookies = () => {
+  setCookie('cookie_preference', encodeURIComponent(JSON.stringify({
+    essential: true,
+    additional: true
+  })), 365);
+  setCookie('cookie_preference_set', true, 365);
+  showConfirmationMessage();
+  confirmationMessageAccept.hidden = false;
+}
+
+const rejectCookies = () => {
+  setCookie('cookie_preference', encodeURIComponent(JSON.stringify(DEFAULT_COOKIE_CONSENT)), 365);
+  setCookie('cookie_preference_set', true, 365);
+  showConfirmationMessage();
+  confirmationMessageReject.hidden = false;
+}
+
+const DEFAULT_COOKIE_CONSENT = {
+  essential: true,
+  additional: false
+}
+
+function hideCookieBanner() {
+  cookieBanner.hidden = true;
+}
+
+function showConfirmationMessage() {
+  cookieSelectionContainer.hidden = true;
+  confirmationMessageContainer.hidden = false;
+  confirmationMessageContainer.focus();
+}
+
+if(getCookie('cookie_preference_set')){
+  hideCookieBanner();
+}else{
+  setCookie('cookie_preference', encodeURIComponent(JSON.stringify(DEFAULT_COOKIE_CONSENT)), 365);
+}
+
+acceptBtn.addEventListener("click", () => {
+  acceptCookies();
+});
+
+rejectBtn.addEventListener("click", () => {
+  rejectCookies();
+});
+
+hideBannerBtn.addEventListener("click", () => {
+  hideCookieBanner();
+});
+console.log("inside the init method");
 
 // need to add gtm classes to button in view, depending on cookie acceptance status
 
