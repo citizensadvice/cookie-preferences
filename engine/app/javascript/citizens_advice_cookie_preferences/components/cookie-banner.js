e98b75c268ef22cbc073f950912ae8ccc333b11e
KPobeeNorris <kath.pobee-norris@citizensadvice.org.uk>
2025-07-24 12:32:29 +0100

    Refactor cookie-banner.js
    We've moved the cookie banner into a components directory and refactored
    the JS to make it easier to read, and make it nice and tidy
    
    Co-authored-by: marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>

--- engine/app/javascript/citizens_advice_cookie_preferences/components/cookie-banner.js
+++ engine/app/javascript/citizens_advice_cookie_preferences/components/cookie-banner.js
@@ -1,11 +1,20 @@
-const cookieBanner = document.querySelector(".js-cookie-banner");
-const acceptBtn = document.querySelector("#js-cookie-banner__button-accept");
-const rejectBtn = document.querySelector("#js-cookie-banner__button-reject");
-const cookieSelectionContainer = document.querySelector(".js-cookie-banner__selection-container");
-const confirmationMessageContainer = document.querySelector(".js-cookie-banner__confirmation-message");
-const confirmationMessageAccept = confirmationMessageContainer.querySelector(".js-cookie-banner__confirmation-accept");
-const confirmationMessageReject = confirmationMessageContainer.querySelector(".js-cookie-banner__confirmation-reject");
-const hideBannerBtn = confirmationMessageContainer.querySelector("#js-cookie-banner__button-hide");
+const selectors = {
+  cookieBanner: ".js-cookie-banner",
+  acceptBtn: "#js-cookie-banner__button-accept",
+  rejectBtn: "#js-cookie-banner__button-reject",
+  cookieSelectionContainer: ".js-cookie-banner__selection-container",
+  confirmationMessageContainer: ".js-cookie-banner__confirmation-message",
+  confirmationMessageAccept: ".js-cookie-banner__confirmation-accept",
+  confirmationMessageReject: ".js-cookie-banner__confirmation-reject",
+  hideBannerBtn: "#js-cookie-banner__button-hide"
+};
+
+const DEFAULT_COOKIE_CONSENT = {
+  essential: true,
+  additional: false
+}
+
+const cookieBanner = document.querySelector(selectors.cookieBanner);
 
 function setCookie(cname, cvalue, exdays) {
   // set expiry date in milliseconds
@@ -37,19 +46,14 @@ const acceptCookies = () => {
   })), 365);
   setCookie('cookie_preference_set', true, 365);
   showConfirmationMessage();
-  confirmationMessageAccept.hidden = false;
+  document.querySelector(selectors.confirmationMessageAccept).hidden = false;
 }
 
 const rejectCookies = () => {
   setCookie('cookie_preference', encodeURIComponent(JSON.stringify(DEFAULT_COOKIE_CONSENT)), 365);
   setCookie('cookie_preference_set', true, 365);
   showConfirmationMessage();
-  confirmationMessageReject.hidden = false;
-}
-
-const DEFAULT_COOKIE_CONSENT = {
-  essential: true,
-  additional: false
+  document.querySelector(selectors.confirmationMessageReject).hidden = false
 }
 
 function hideCookieBanner() {
@@ -57,7 +61,8 @@ function hideCookieBanner() {
 }
 
 function showConfirmationMessage() {
-  cookieSelectionContainer.hidden = true;
+  const confirmationMessageContainer = document.querySelector(selectors.confirmationMessageContainer);
+  document.querySelector(selectors.cookieSelectionContainer).hidden = true;
   confirmationMessageContainer.hidden = false;
   confirmationMessageContainer.focus();
   cookieBanner.classList.add("cookie-banner--no-decoration");
@@ -71,20 +76,26 @@ function setDefaultCookies() {
     setCookie('cookie_preference', encodeURIComponent(JSON.stringify(DEFAULT_COOKIE_CONSENT)), 365);
   }
 }
-export default function initCookieBanner() {
-  setDefaultCookies();
-  acceptBtn.addEventListener("click", () => {
+
+function addCookieBannerEventHandlers() {
+  document.querySelector(selectors.acceptBtn).addEventListener("click", () => {
     acceptCookies();
   });
 
-  rejectBtn.addEventListener("click", () => {
+  document.querySelector(selectors.rejectBtn).addEventListener("click", () => {
     rejectCookies();
   });
 
-  hideBannerBtn.addEventListener("click", () => {
+  document.querySelector(selectors.hideBannerBtn).addEventListener("click", () => {
     hideCookieBanner();
   });
 }
+
+export default function initCookieBanner() {
+  setDefaultCookies();
+  addCookieBannerEventHandlers();
+}
+
 // need to add gtm classes to button in view, depending on cookie acceptance status
 
 // need datalayer push for accept & reject cookies
