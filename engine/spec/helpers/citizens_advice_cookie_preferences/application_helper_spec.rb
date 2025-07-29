1f54aa54529abc4bee5c771da641810c0af88969
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2025-07-29 16:52:08 +0100

    feat: refactor application helper spec Now stubbing CurrentCookies analytics? and video_players? methods instead of directly calling them.
    Co-authored-by: marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>

--- engine/spec/helpers/citizens_advice_cookie_preferences/application_helper_spec.rb
+++ engine/spec/helpers/citizens_advice_cookie_preferences/application_helper_spec.rb
@@ -4,64 +4,30 @@ require "rails_helper"
 
 RSpec.describe CitizensAdviceCookiePreferences::ApplicationHelper do
   describe "#allow_analytics_cookies?" do
-    it "returns true when analytics cookies have been accepted" do
-      accept_additional_cookies
-      cookies_preference_parsed = JSON.parse(cookies[:cookie_preference])
-      CitizensAdviceCookiePreferences::CurrentCookies.preference = cookies_preference_parsed
+    it "returns true when CurrentCookies.analytics? is true" do
+      allow(CitizensAdviceCookiePreferences::CurrentCookies).to receive(:analytics?).and_return(true)
 
       expect(helper.allow_analytics_cookies?).to be(true)
     end
 
-    it "returns false when analytics cookies have been rejected" do
-      reject_additional_cookies
-      cookies_preference_parsed = JSON.parse(cookies[:cookie_preference])
-      CitizensAdviceCookiePreferences::CurrentCookies.preference = cookies_preference_parsed
+    it "returns false when CurrentCookies.analytics? is false" do
+      allow(CitizensAdviceCookiePreferences::CurrentCookies).to receive(:analytics?).and_return(false)
 
-      expect(helper.allow_analytics_cookies?).to be(false)
-    end
-
-    it "returns false if the analytics cookies have not been set" do
       expect(helper.allow_analytics_cookies?).to be(false)
     end
   end
 
   describe "#allow_video_players_cookies?" do
-    it "returns true when video_players cookies have been accepted" do
-      accept_additional_cookies
-      cookies_preference_parsed = JSON.parse(cookies[:cookie_preference])
-      CitizensAdviceCookiePreferences::CurrentCookies.preference = cookies_preference_parsed
+    it "returns true when CurrentCookies.video_players? is true" do
+      allow(CitizensAdviceCookiePreferences::CurrentCookies).to receive(:video_players?).and_return(true)
 
       expect(helper.allow_video_players_cookies?).to be(true)
     end
 
-    it "returns false when video_players cookies have been rejected" do
-      reject_additional_cookies
-      cookies_preference_parsed = JSON.parse(cookies[:cookie_preference])
-      CitizensAdviceCookiePreferences::CurrentCookies.preference = cookies_preference_parsed
+    it "returns false when CurrentCookies.video_players? is false" do
+      allow(CitizensAdviceCookiePreferences::CurrentCookies).to receive(:video_players?).and_return(false)
 
       expect(helper.allow_video_players_cookies?).to be(false)
     end
-
-    it "returns false if the video_players cookies have not been set" do
-      expect(helper.allow_video_players_cookies?).to be(false)
-    end
-  end
-
-  private
-
-  def accept_additional_cookies
-    cookies[:cookie_preference] = {
-      value: { essential: true, analytics: true, video_players: true }.to_json,
-      expires: 1.year,
-      domain: :all
-    }
-  end
-
-  def reject_additional_cookies
-    cookies[:cookie_preference] = {
-      value: { essential: true, analytics: false, video_players: false }.to_json,
-      expires: 1.year,
-      domain: :all
-    }
   end
 end
