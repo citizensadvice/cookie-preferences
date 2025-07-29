25785e6fa8ca88f7aba9bf14bb447b3275fa4b82
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2025-07-29 11:49:46 +0100

    feat: adding cookie preference helpers in the demo app.
    This is to help us test it
    
    Co-authored-by: Lucy Stringer <lucy.stringer@citizensadvice.org.uk>

--- demo/app/controllers/application_controller.rb
+++ demo/app/controllers/application_controller.rb
@@ -2,6 +2,8 @@
 
 class ApplicationController < ActionController::Base
   include CitizensAdviceComponents::Helpers
+  helper CitizensAdviceCookiePreferences::ApplicationHelper
+  include CitizensAdviceCookiePreferences::CookiePreferencesHelpers
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
