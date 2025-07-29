619c5743af08f520695c2ef230dc6651be9f34f4
KPobeeNorris <kath.pobee-norris@citizensadvice.org.uk>
2025-07-29 12:11:29 +0100

    Add helper method so apps that mount the engine don't render the banner on the cookies preferences page

--- engine/app/helpers/citizens_advice_cookie_preferences/application_helper.rb
+++ engine/app/helpers/citizens_advice_cookie_preferences/application_helper.rb
@@ -2,5 +2,8 @@
 
 module CitizensAdviceCookiePreferences
   module ApplicationHelper
+    def cookies_preferences_page?
+      request.path == "/cookie-preferences/cookie_preference/edit"
+    end
   end
 end
