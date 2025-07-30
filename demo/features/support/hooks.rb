1b536670f5b568b7e7c9c46cbf688d6f5322dcc5
KPobeeNorris <kath.pobee-norris@citizensadvice.org.uk>
2025-07-30 14:22:37 +0100

    Get artifacts uploading

--- demo/features/support/hooks.rb
+++ demo/features/support/hooks.rb
@@ -2,7 +2,7 @@
 
 After do |test_case|
   if test_case.failed?
-    path = "demo/cucumber-results/screenshots/#{test_case.name}.png"
+    path = "cucumber-results/screenshots/#{test_case.name}.png"
     Rails.log.info("Taking a screenshot to #{path}")
     Capybara.current_session.save_screenshot(path)
     attach(path, "image/png")
