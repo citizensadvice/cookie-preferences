3de7662c65c5e3cc07b3dd394ec33db7576c395d
KPobeeNorris <kath.pobee-norris@citizensadvice.org.uk>
2025-07-30 16:35:17 +0100

    Remove unused code

--- engine/config/routes.rb
+++ engine/config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 CitizensAdviceCookiePreferences::Engine.routes.draw do
-  # root to: "cookie_preferences_page#index"
-  # root to: "cookie_preferences#edit"
   root to: "/"
   resource :cookie_preference, only: %i[show edit update]
 end
