53cb1abcc97f44c89e10f2976f0cc8c3c9fe138e
KPobeeNorris <kath.pobee-norris@citizensadvice.org.uk>
2025-07-31 16:00:18 +0100

    Spike into exploring best approach for handling assets when mounting the engine in other apps

--- engine/lib/citizens_advice_cookie_preferences/engine.rb
+++ engine/lib/citizens_advice_cookie_preferences/engine.rb
@@ -3,5 +3,9 @@
 module CitizensAdviceCookiePreferences
   class Engine < ::Rails::Engine
     isolate_namespace CitizensAdviceCookiePreferences
+
+    initializer "citizens_advice_cookie_preferences.assets.precompile" do |app|
+      app.config.assets.precompile += %w( cookies.css cookies.js )
+    end
   end
 end
