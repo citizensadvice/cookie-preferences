dc537156d93d8a7b16a032baa94285727c4a768e
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2025-08-12 11:03:28 +0100

    feat: add gtm button classes for accept & reject cookies on banner & inject GTM on acceptance Adds gtm button classes for accept & reject cookies via the cookie banner. Also injects GTM snippet on cookie acceptance. The button classes were intended to link to GTM triggers, but may not be needed as GTM is not activated until after the accept cookies button click.
    Co-authored-by: marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
    Co-authored-by: Kath <kath.pobee-norris@citizensadvice.org.uk>
    Co-authored-by: Leeky <robert.lee-cann@citizensadvice.org.uk>

--- engine/app/components/citizens_advice_cookie_preferences/cookie_banner.rb
+++ engine/app/components/citizens_advice_cookie_preferences/cookie_banner.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
 module CitizensAdviceCookiePreferences
-  class CookieBanner < ViewComponent::Base; end
+  class CookieBanner < ViewComponent::Base
+    def accept_all_cookies_button_classes
+      base_button_classes << "gtm-accept-all-cookies"
+    end
+
+    def reject_all_cookies_button_classes
+      base_button_classes << "gtm-reject-all-cookies"
+    end
+
+    def base_button_classes
+      %w[
+      cads-button
+      cads-button__primary
+    ]
+    end
+  end
 end
