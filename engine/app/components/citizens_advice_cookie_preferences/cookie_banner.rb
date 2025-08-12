88c71588556fed610f485a0c0524575bacde8109
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2025-08-12 11:33:38 +0100

    feat: removing the gtm classes from accept and reject banner buttons
    We pass a custom event in js instead
    
    Co-authored-by: Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
    Co-authored-by: Kath <kath.pobee-norris@citizensadvice.org.uk>

--- engine/app/components/citizens_advice_cookie_preferences/cookie_banner.rb
+++ engine/app/components/citizens_advice_cookie_preferences/cookie_banner.rb
@@ -2,19 +2,11 @@
 
 module CitizensAdviceCookiePreferences
   class CookieBanner < ViewComponent::Base
-    def accept_all_cookies_button_classes
-      base_button_classes << "gtm-accept-all-cookies"
-    end
-
-    def reject_all_cookies_button_classes
-      base_button_classes << "gtm-reject-all-cookies"
-    end
-
     def base_button_classes
       %w[
-      cads-button
-      cads-button__primary
-    ]
+        cads-button
+        cads-button__primary
+      ]
     end
   end
 end
