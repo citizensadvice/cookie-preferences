fd01c6bdc33357bc0ee8b4ebaabb39372fbdfd2d
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2025-08-13 11:22:01 +0100

    feat: restructuring js files, introduced a separate cookie-preferences-page file and moved the analytics scripts into a separate helpers file.

--- engine/app/javascript/citizens_advice_cookie_preferences/components/cookie-preferences-page.js
+++ engine/app/javascript/citizens_advice_cookie_preferences/components/cookie-preferences-page.js
@@ -1,10 +1,17 @@
-export default function initCookiePreferencesPage() {
-  const form = document.querySelector('.cookie-preferences-form');
-  const submitButton = form.querySelector('button')
+function addCookiePreferencePageEventHandler() {
+  const form = document.querySelector(".edit_cookie_preference");
+  const submitButton = form?.querySelector("button");
+
+  if (form && submitButton) {
+    submitButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      // some code goes here
+      console.log("This is test from a differet file");
+      form.submit();
+    });
+  }
 }
 
-// add an event listener on the click
-// do a console log
-// aim is to stop the default rails behaviour on the click, do what we want, then continue with the rails submit behaviour
-// MY expects it to work up until us adding analytics work, but doesn't know if the form.submit will now work properly or not.
-// put it all in init function above. Could even put it all in the cookie-banner file for testing purposes, then move it out later if it works.
+export default function initCookiePreferencesPage() {
+  addCookiePreferencePageEventHandler();
+}
