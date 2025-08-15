e44a008ee3c911ba4973f5fcee8256bf9685acd7
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2025-08-15 17:44:10 +0100

    WIP: refactor to use full cookies list On reset cookies compare cookies from browser against full list of essential & analytics cookies, with a view to future use.
    Co-authored-by: Leeky <robert.lee-cann@citizensadvice.org.uk>

--- demo/features/step_definitions/cookie_helpers_test_steps.rb
+++ demo/features/step_definitions/cookie_helpers_test_steps.rb
@@ -11,3 +11,26 @@ end
 Then("I see a message that the {string} cookies are rejected") do |text|
   expect(page).to have_text("This text appears if the #{text} cookies have been rejected.")
 end
+
+And("I have a boatload of cookies") do
+  page.driver.browser.manage.add_cookie({ name: "mouseflow", value: "mouseflow_cookie" })
+  page.driver.browser.manage.add_cookie({ name: "mf_1", value: "mouseflow_cookie_1" })
+  page.driver.browser.manage.add_cookie({ name: "_ga_135m04mh2", value: "ga_cookie" })
+  page.driver.browser.manage.add_cookie({ name: "ar_debug", value: "ar_debug_cookie" })
+  page.driver.browser.manage.add_cookie({ name: "ethnio_displayed", value: "ethnio_cookie" })
+  page.driver.browser.manage.add_cookie({ name: "geo", value: "geo_cookie" })
+  page.driver.browser.manage.add_cookie({ name: ".CitizensAdviceLogin", value: "legacy_login_cookie" })
+end
+
+Then("the non-essential cookies are deleted") do
+  expect(page.driver.browser.manage.all_cookies.find { |cookie| cookie[:name] == "mouseflow" }).to be_nil
+  expect(page.driver.browser.manage.all_cookies.find { |cookie| cookie[:name] == "ethnio_displayed" }).to be_nil
+end
+
+And("the essential cookies are not deleted") do
+  expect(page.driver.browser.manage.all_cookies.find { |cookie| cookie[:name] == "cookie_preference" }).not_to be_nil
+end
+
+And("the non-approved cookies are deleted") do
+  expect(page.driver.browser.manage.all_cookies.find { |cookie| cookie[:name] == ".CitizensAdviceLogin" }).to be_nil
+end
