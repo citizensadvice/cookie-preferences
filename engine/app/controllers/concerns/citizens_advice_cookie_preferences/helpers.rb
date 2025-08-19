6ba1a637fb861bc5241564b28d8a199a600592ba
KPobeeNorris <kath.pobee-norris@citizensadvice.org.uk>
2025-08-19 14:20:46 +0100

    Start refactoring deletion non-consented cookies
    Extract our methods into a new class, that we're currently called
    CookieManagement until we find a better name!

--- engine/app/controllers/concerns/citizens_advice_cookie_preferences/helpers.rb
+++ engine/app/controllers/concerns/citizens_advice_cookie_preferences/helpers.rb
@@ -58,37 +58,49 @@ module CitizensAdviceCookiePreferences
       # Delete cookie_preference_set so that banner is rendered and a user can re-consent
       cookies.delete :cookie_preference_set
 
-      category_consent = JSON.parse(cookies[:cookie_preference])
-      wildcard_cookies = COOKIE_CATEGORIES.select { |name| name.end_with?("*") }.keys
+      CookieManagement.new(cookies).delete_unconsented_cookies!
+    end
 
-      # rubocop:disable Style/HashEachMethods
-      cookies.each do |cookie, _|
-        # Find category where cookie name exactly matches
-        category = COOKIE_CATEGORIES[cookie]
+    class CookieManagement
+      attr_reader :cookies
 
-        # Find category where cookie name matches a wildcard
-        if category.nil?
-          matched_wildcard_cookie = wildcard_cookies.detect do |wildcard_cookie|
-            start_string = wildcard_cookie.to_s[0..-2]
-
-            cookie.start_with?(start_string)
-          end
-
-          if matched_wildcard_cookie.nil?
-            cookies.delete(cookie)
-            next
-          end
-
-          category = COOKIE_CATEGORIES[matched_wildcard_cookie]
-        end
-
-        # ignore 'essential' cookies
-        next if category == "essential"
-
-        # delete unconsented cookies
-        cookies.delete(cookie) unless category_consent[category]
+      def initialize(cookies)
+        @cookies = cookies
       end
-      # rubocop:enable Style/HashEachMethods
+
+      def delete_unconsented_cookies!
+        category_consent = JSON.parse(cookies[:cookie_preference])
+        wildcard_cookies = COOKIE_CATEGORIES.select { |name| name.end_with?("*") }.keys
+
+        # rubocop:disable Style/HashEachMethods
+        cookies.each do |cookie, _|
+          # Find category where cookie name exactly matches
+          category = COOKIE_CATEGORIES[cookie]
+
+          # Find category where cookie name matches a wildcard
+          if category.nil?
+            matched_wildcard_cookie = wildcard_cookies.detect do |wildcard_cookie|
+              start_string = wildcard_cookie.to_s[0..-2]
+
+              cookie.start_with?(start_string)
+            end
+
+            if matched_wildcard_cookie.nil?
+              cookies.delete(cookie)
+              next
+            end
+
+            category = COOKIE_CATEGORIES[matched_wildcard_cookie]
+          end
+
+          # ignore 'essential' cookies
+          next if category == "essential"
+
+          # delete unconsented cookies
+          cookies.delete(cookie) unless category_consent[category]
+        end
+        # rubocop:enable Style/HashEachMethods
+      end
     end
   end
 end
