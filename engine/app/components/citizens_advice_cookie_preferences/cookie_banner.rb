2ab3fb9e64ad1482da83f27b4c3c23179755576e
Stacy Hayes <stacy.hayes@citizensadvice.org.uk>
2025-09-03 15:39:50 +0100

    Conditionally add country to banner link
    We want to account for `/wales` and `/scotland`, this adds
    the functionality to link to them from the banner on wales
    or scotland pages, and to apply scotland stlying to the
    scotland pref page

--- engine/app/components/citizens_advice_cookie_preferences/cookie_banner.rb
+++ engine/app/components/citizens_advice_cookie_preferences/cookie_banner.rb
@@ -2,6 +2,14 @@
 
 module CitizensAdviceCookiePreferences
   class CookieBanner < ViewComponent::Base
+
+    def initialize(current_country:)
+      @current_country = current_country
+    end
+
+    def country_url
+      "/#{@current_country}/cookie-preferences"
+    end
     def base_button_classes
       %w[
         cads-button
