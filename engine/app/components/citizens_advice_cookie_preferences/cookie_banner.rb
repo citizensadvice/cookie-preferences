fc69b6e34c879f07563a75301bab4300c13fb2b3
KPobeeNorris <kath.pobee-norris@citizensadvice.org.uk>
2025-09-04 08:19:43 +0100

    chore/Update the how we use cookies link in the banner
    Now we've got country specific routing we also need to update the link
    to the "how we use cookies" page to ensure users are sent to the correct
    version of the page

--- engine/app/components/citizens_advice_cookie_preferences/cookie_banner.rb
+++ engine/app/components/citizens_advice_cookie_preferences/cookie_banner.rb
@@ -2,14 +2,17 @@
 
 module CitizensAdviceCookiePreferences
   class CookieBanner < ViewComponent::Base
+    delegate :how_we_use_cookies_url, to: :helpers
 
     def initialize(current_country:)
+      super()
       @current_country = current_country.downcase
     end
 
     def country_url
       @current_country == "england" ? "/cookie-preferences" : "/#{@current_country}/cookie-preferences"
     end
+
     def base_button_classes
       %w[
         cads-button
--- engine/app/controllers/concerns/citizens_advice_cookie_preferences/helpers.rb
+++ engine/app/controllers/concerns/citizens_advice_cookie_preferences/helpers.rb
@@ -11,7 +11,7 @@ module CitizensAdviceCookiePreferences
 
     included do
       before_action :set_cookie_preferences, :check_cookie_version
-      helper_method :cookies_preference_page?, :allow_analytics_cookies?, :allow_video_players_cookies?
+      helper_method :cookies_preference_page?, :allow_analytics_cookies?, :allow_video_players_cookies?, :how_we_use_cookies_url
 
       def cookies_preference_page?
         false
@@ -23,6 +23,16 @@ module CitizensAdviceCookiePreferences
 
       def allow_video_players_cookies?
         CitizensAdviceCookiePreferences::CurrentCookies.video_players?
+      end
+
+      def how_we_use_cookies_url(country)
+        if country == "scotland"
+          "/scotland/about-us/information/privacy-and-cookies-scotland/"
+        elsif country == "wales"
+          "/wales/about-us/information/how-we-use-cookies/"
+        else
+          "/about-us/information/how-we-use-cookies/"
+        end
       end
     end
 
