10fadf2e774dff09a3a35e6e1484ffe2dd20ae14
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2025-09-23 16:11:15 +0100

    spike: adding a return url to the cookie preferences page
    Co-authored-by: Stacy Hayes <stacy.hayes@citizensadvice.org.uk>

--- engine/app/controllers/citizens_advice_cookie_preferences/cookie_preferences_controller.rb
+++ engine/app/controllers/citizens_advice_cookie_preferences/cookie_preferences_controller.rb
@@ -10,10 +10,12 @@ module CitizensAdviceCookiePreferences
     before_action :set_default_cookie, only: :edit
 
     def show
-      redirect_to localised_engine_namespace.edit_cookie_preference_path, country: params[:country]
+      redirect_to localised_engine_namespace.edit_cookie_preference_path(country: params[:country],
+                                                                         cookie_prefs_return_url: cookie_prefs_return_url)
     end
 
     def edit
+      @return_url = formatted_return_url(cookie_prefs_return_url)
       @localised_engine_namespace = localised_engine_namespace
       @current_country = params[:country]
       @page_title = t("cookie_preferences.title")
@@ -29,7 +31,6 @@ module CitizensAdviceCookiePreferences
         update_cookie_preferences
         CookieManagement.new(cookies).delete_unconsented_cookies!
         flash[:notice] = t("cookie_preferences.update.success")
-
         redirect_to localised_engine_namespace.edit_cookie_preference_path
       else
         render :edit
@@ -38,6 +39,46 @@ module CitizensAdviceCookiePreferences
     # rubocop:enable Metrics/AbcSize
 
     private
+
+    def cookie_prefs_return_url
+      params[:cookie_prefs_return_url] || request.referer
+
+      if params[:cookie_prefs_return_url]
+        params[:cookie_prefs_return_url]
+      elsif valid_referer?
+        request.referer
+      end
+    end
+
+    def valid_referer?
+      return false unless request.referer
+
+      # First part is localhost
+      # Second part is any CA subdomain
+      uri = URI.parse(request.referer)
+      return true if uri.host == request.host || uri.host.include?("citizensadvice.org.uk")
+
+      false
+    end
+
+    def formatted_return_url(url)
+      return unless url
+
+      uri = URI.parse(url)
+
+      if uri.query
+        # When the back link is coming form request.referer rather than a param,
+        # we need to extract it from the url param
+        # E.g http://localhost:3000/cookie-preferences/edit?cookie_prefs_return_url=http%3A%2F%2Flocalhost%3A3000%2Fshow-page
+        CGI.parse(uri.query)["cookie_prefs_return_url"][0]
+
+      else
+        # Don't return a formatted url if the user visited the cookie preferences page directly
+        return if uri.path == localised_engine_namespace.edit_cookie_preference_path
+
+        url
+      end
+    end
 
     def prefs_from_form
       params.fetch(:cookie_preference).permit(:analytics, :video_players)
