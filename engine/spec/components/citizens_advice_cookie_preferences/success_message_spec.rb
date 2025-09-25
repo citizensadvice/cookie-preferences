0496938c56d60e1dc04371c19c7a5774dc4f0b93
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2025-09-25 15:39:40 +0100

    feat: success message component spec
    Uses the design system success message component spec as a base, but adds the return url check

---
+++ engine/spec/components/citizens_advice_cookie_preferences/success_message_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe CitizensAdviceCookiePreferences::SuccessMessage, type: :component do
+  subject { page }
+
+  context "with default arguments" do
+    before do
+      render_inline described_class.new(message: "Your cookie settings have been saved")
+    end
+
+    it { is_expected.to have_css ".cads-success-message", text: "Your cookie settings have been saved" }
+
+    it { is_expected.to have_css "[aria-live=polite]" }
+
+    it { is_expected.to have_no_link "Go back to the page you were looking at" }
+  end
+
+  context "with cookie_prefs_return_url" do
+    before do
+      render_inline described_class.new(message: "Your cookie settings have been saved", cookie_prefs_return_url: "/some-page")
+    end
+
+    it { is_expected.to have_link "Go back to the page you were looking at", href: "/some-page" }
+  end
+
+  context "when no message present" do
+    before { render_inline described_class.new(message: nil) }
+
+    it { is_expected.to have_no_css ".cads-success-message" }
+  end
+end
