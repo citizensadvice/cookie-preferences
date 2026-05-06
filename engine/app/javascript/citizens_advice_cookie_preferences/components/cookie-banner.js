705651ccd1ff6375d3d02fc0beb27affbbbf4f48
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2026-05-06 18:02:51 +0100

    feat: separating cookie functions into a new helper file
    This will help us in later steps when we add more cookie functions

--- engine/app/javascript/citizens_advice_cookie_preferences/components/cookie-banner.js
+++ engine/app/javascript/citizens_advice_cookie_preferences/components/cookie-banner.js
@@ -1,4 +1,5 @@
 import { acceptCookiesGTMEvent } from "../helpers/analytics";
+import { setCookie, getCookie } from "../helpers/cookie-functions"
 
 const selectors = {
   cookieBanner: ".js-cookie-banner",
@@ -16,41 +17,6 @@ const DEFAULT_COOKIE_CONSENT = {
   analytics: false,
   video_players: false,
 };
-
-const cookieDomain =
-  document.location.hostname === "localhost"
-    ? "localhost"
-    : "citizensadvice.org.uk";
-
-function setCookie(cname, cvalue, exdays) {
-  // set expiry date in milliseconds
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.cookie =
-    cname +
-    "=" +
-    cvalue +
-    ";" +
-    expires +
-    ";path=/" +
-    `;domain=${cookieDomain}`;
-}
-
-function getCookie(name) {
-  const nameEQ = `${name}=`;
-  const cookies = document.cookie.split(";");
-  for (let i = 0, len = cookies.length; i < len; i++) {
-    let cookie = cookies[i];
-    while (cookie.charAt(0) === " ") {
-      cookie = cookie.substring(1, cookie.length);
-    }
-    if (cookie.indexOf(nameEQ) === 0) {
-      return decodeURIComponent(cookie.substring(nameEQ.length));
-    }
-  }
-  return null;
-}
 
 const acceptCookies = () => {
   setCookie(
