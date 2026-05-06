98c0d32dedee6862d25e72a5d7218013222863e4
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2026-05-06 18:07:41 +0100

    feat: adding functions to remove cookies on reject cookies button click

--- engine/app/javascript/citizens_advice_cookie_preferences/helpers/cookie-functions.js
+++ engine/app/javascript/citizens_advice_cookie_preferences/helpers/cookie-functions.js
@@ -1,7 +1,16 @@
+import { COOKIE_CATEGORIES } from "../constants/cookie-categories";
+
 const cookieDomain =
   document.location.hostname === "localhost"
     ? "localhost"
     : "citizensadvice.org.uk";
+
+const consentedCategories = () => {
+  const cookiePreference = JSON.parse(getCookie("cookie_preference"));
+  return Object.keys(cookiePreference).filter(
+    (key) => cookiePreference[key] === true,
+  );
+};
 
 export function setCookie(cname, cvalue, exdays) {
   // set expiry date in milliseconds
@@ -31,4 +40,53 @@ export function getCookie(name) {
     }
   }
   return null;
+}
+
+function permittedCookie(cookie) {
+  let consented = consentedCategories();
+
+  if (Object.prototype.hasOwnProperty.call(COOKIE_CATEGORIES, cookie)) {
+    return consented.includes(COOKIE_CATEGORIES[cookie]);
+  }
+
+  // Wildcard cookies
+  const wildcardMatch = Object.keys(COOKIE_CATEGORIES).find((key) => {
+    return key.endsWith("*") && cookie.startsWith(key.slice(0, -1));
+  });
+
+  if (wildcardMatch) {
+    const category = COOKIE_CATEGORIES[wildcardMatch];
+    return consented.includes(category);
+  }
+
+  return false;
+}
+
+function expireCookie(cookie, value = "") {
+  // We need to handle deleting cookies on the domain and the .domain
+  let thePast = new Date(0); // 0 = 0 seconds since UTC started (1970/01/01)
+  document.cookie = cookie + "=" + value + ";expires=" + thePast + ";";
+  document.cookie =
+    cookie +
+    "=" +
+    value +
+    ";expires=" +
+    thePast +
+    `;domain=${cookieDomain};path=/`;
+}
+
+export function deleteUnconsentedCookies() {
+  // We can only access and expire same domain cookies
+  document.cookie
+    .split(";")
+    .map((cookie) => cookie.trim())
+    .filter(Boolean) // Remove empty strings
+    .forEach((cookieString) => {
+      // Split name from value safely
+      const cookieName = cookieString.split("=")[0];
+
+      if (!permittedCookie(cookieName)) {
+        expireCookie(cookieName);
+      }
+    });
 }
